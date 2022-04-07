0cf939c25822d9d976574e3e9c340076cce03057
superrache@hotmail.fr <superrache@hotmail.fr>
2022-04-07 19:54:38 +0200

    feat: node memory monitoring in logs

--- server/api.js
+++ server/api.js
@@ -8,11 +8,22 @@ module.exports = function(app, prod) {
 
     const simpleCache = []
 
+    const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`
+
     app.get('/data', (req, res) => {
         if(!prod) { // parce que les ports server vue et server node sont différents en dev
             res.header('Access-Control-Allow-Origin', "*")
             res.header('Access-Control-Allow-Headers', "*")
         }
+
+        const memoryData = process.memoryUsage()
+        const memoryUsage = {
+            rss: `${formatMemoryUsage(memoryData.rss)} -> Resident Set Size - total memory allocated for the process execution`,
+            heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the allocated heap`,
+            heapUsed: `${formatMemoryUsage(memoryData.heapUsed)} -> actual memory used during the execution`,
+            external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
+        }
+        console.log(memoryUsage)
 
         try {
             const instances = [/*'https://overpass-api.de', */'https://lz4.overpass-api.de', 'https://z.overpass-api.de']
