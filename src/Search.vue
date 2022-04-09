4494776dc738fadd2513198b1f2e2477616ce0f5
superrache@hotmail.fr <superrache@hotmail.fr>
2022-04-09 14:06:30 +0200

    feat: search input keyup.enter and clear button

--- src/Search.vue
+++ src/Search.vue
@@ -1,19 +1,32 @@
 <template>
     <div class="cat">
-        <input id="input"
-            @input="updateInput"
-            placeholder="Rechercher"
-            type="search"
-            autofocus
-        />
-        <div id="results" v-if="results.length > 0">
-            <div class="result"
-                v-for="r in results"
-                :key="r.place_id"
-                v-on:click="onResultClic($event, r)">
-                {{r.display_name}}
+        <div>
+            <input id="input"
+                ref="input"
+                @input="updateInput"
+                @keyup.enter="enter"
+                v-model="inputStr"
+                placeholder="Rechercher"
+                type="search"
+                autofocus
+            />
+            <div id="results" v-if="results.length > 0">
+                <div class="result"
+                    v-for="r in results"
+                    :key="r.place_id"
+                    @click="onResultClic($event, r)">
+                    {{r.display_name}}
+                </div>
             </div>
         </div>
+        <button id="clear" @click="clear">
+<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="52px" height="52px" viewBox="0 0 52 52" enable-background="new 0 0 52 52" fill="#ccc" xml:space="preserve">
+<path d="M26,2C12.7,2,2,12.7,2,26s10.7,24,24,24s24-10.7,24-24S39.3,2,26,2z M30.9,26.8l7.8,7.8c0.4,0.4,0.4,1,0,1.4
+l-2.8,2.8c-0.4,0.4-1,0.4-1.4,0L26.7,31c-0.4-0.4-1-0.4-1.4,0l-7.8,7.8c-0.4,0.4-1,0.4-1.4,0L13.3,36c-0.4-0.4-0.4-1,0-1.4l7.8-7.8
+c0.4-0.4,0.4-1,0-1.4l-7.9-7.9c-0.4-0.4-0.4-1,0-1.4l2.8-2.8c0.4-0.4,1-0.4,1.4,0l7.9,7.9c0.4,0.4,1,0.4,1.4,0l7.8-7.8
+c0.4-0.4,1-0.4,1.4,0l2.8,2.8c0.4,0.4,0.4,1,0,1.4l-7.8,7.8C30.6,25.8,30.6,26.4,30.9,26.8z"/>
+</svg>
+        </button>
     </div>
 </template>
 
@@ -26,7 +39,8 @@ export default {
   data() {
     return {
         map: null,
-        results: []
+        results: [],
+        inputStr: ''
     }
   },
   methods: {
@@ -42,6 +56,19 @@ export default {
     onResultClic(e, result) {
         this.map.flyTo([result.lon, result.lat])
         this.results = []
+    },
+    enter() { // entrer => premier résultat
+    console.log('enter')
+        if(this.results.length > 0) {
+            let result = this.results[0]
+            this.map.flyTo([result.lon, result.lat])
+            this.results = []
+        }
+    },
+    clear() {
+        this.inputStr = ''
+        this.results = []
+        this.$refs.input.focus()
     }
   }
 }
@@ -54,8 +81,9 @@ export default {
   background-color: #444;
   border-radius: 10px;
   padding: 7px;
-  padding-right: 15px;
   margin: 5px 0px 10px 0px;
+  display: flex;
+  justify-content:right;
 }
 
 #input {
@@ -65,6 +93,20 @@ export default {
     border-radius: 5px;
     background-color: #444;
     color: white;
+}
+
+#clear {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    margin: 4px;
+    padding: 1px;
+}
+
+#clear svg {
+    width: 18px;
+    height: 18px;
 }
 
 #input:hover, #input:active {
