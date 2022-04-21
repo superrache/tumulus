91014725ae4098dd5be8ba577ce7a089a1883f25
superrache@hotmail.fr <superrache@hotmail.fr>
2022-04-21 11:08:05 +0200

    fix: correct mhs issue

--- src/issues/Mhs.js
+++ src/issues/Mhs.js
@@ -2,47 +2,44 @@ export function detect(feature, theme) {
     const props = feature.properties
     const issues = []
 
-    for(let key in props) {
-        if(key['ref:mhs'] && !key['heritage:operator']) {
-            issues.push({
-                feature: feature,
-                theme: theme,
-                importance: 1,
-                message: 'ref:mhs sans heritage:operator=mhs',
-                autoRepair: async () => {
-                    feature.properties['heritage:operator'] = 'mhs'
-                    return feature
-                }
-            })
-        }
+    if(props['ref:mhs'] && !props['heritage:operator']) {
+        issues.push({
+            feature: feature,
+            theme: theme,
+            importance: 1,
+            message: 'ref:mhs sans heritage:operator=mhs',
+            autoRepair: async () => {
+                feature.properties['heritage:operator'] = 'mhs'
+                return feature
+            }
+        })
+    }
 
-        if(key['ref:mhs'] && !key['heritage']) {
-            issues.push({
-                feature: feature,
-                theme: theme,
-                importance: 0,
-                message: 'ref:mhs sans heritage=2 (classé monument historique) heritage=3 (inscrit monument historique)'
-            })
-        }
+    if(props['ref:mhs'] && !props['heritage']) {
+        issues.push({
+            feature: feature,
+            theme: theme,
+            importance: 0,
+            message: 'ref:mhs sans heritage=2 (classé monument historique) heritage=3 (inscrit monument historique)'
+        })
+    }
 
-        if(key['ref:mhs'] && !key['mhs:inscription']) {
-            issues.push({
-                feature: feature,
-                theme: theme,
-                importance: 0,
-                message: 'ref:mhs sans date mhs:inscription'
-            })
-        }
+    if(props['ref:mhs'] && !props['mhs:inscription']) {
+        issues.push({
+            feature: feature,
+            theme: theme,
+            importance: 0,
+            message: 'ref:mhs sans date mhs:inscription'
+        })
+    }
 
-        if(key['mhs:inscription'] && !key['ref:mhs']) {
-            issues.push({
-                feature: feature,
-                theme: theme,
-                importance: 0,
-                message: 'mhs:inscription sans ref:mhs'
-            })
-        }
-
+    if(props['mhs:inscription'] && !props['ref:mhs']) {
+        issues.push({
+            feature: feature,
+            theme: theme,
+            importance: 0,
+            message: 'mhs:inscription sans ref:mhs'
+        })
     }
 
     return issues
