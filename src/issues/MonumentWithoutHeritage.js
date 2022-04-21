78a993be8de02a4eede589ea20fe6df7024c1b54
superrache@hotmail.fr <superrache@hotmail.fr>
2022-04-21 11:07:52 +0200

    feat: add issue monument without heritage

---
+++ src/issues/MonumentWithoutHeritage.js
@@ -0,0 +1,17 @@
+export function detect(feature, theme) {
+    const props = feature.properties
+    const issues = []
+
+    if(props['historic'] && props.historic === 'monument') {
+        if(!props('heritage')) {
+            issues.push({
+                feature: feature,
+                theme: theme,
+                importance: 1,
+                message: 'historic=monument sans la série d\'attributs heritage:*'
+            })
+        }
+    }
+
+    return issues
+}
