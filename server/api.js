e1e56b74b4e9f0f5c99a8f02b8427111d2edcbdd
superrache <kevinlecocq@kevin.home>
2023-04-25 21:46:35 +0200

    feat: run server without db

--- server/api.js
+++ server/api.js
@@ -3,31 +3,34 @@
  * @param {app, databaseUrl, prod} app 
  */
 module.exports = function(app, databaseUrl, prod) {
-    console.log('Database URL: ' + databaseUrl)
-    if(databaseUrl === undefined) {
-        console.log('Error: database URL must be specified')
-        process.exit(1)
-    }
+    
+    let db = null
+    
+    if(databaseUrl === undefined || databaseUrl.length == 0) {
+        console.log('Warning: database URL must be specified')
+        console.log('Launching server without database')
+    } else {
+        console.log('Initializing database: ' + databaseUrl)
+        const { Client } = require('pg')
+        
+        let ssl = prod ? {rejectUnauthorized: false} : false
 
-    console.log('Initializing database')
-    const { Client } = require('pg')
-
-    let ssl = prod ? {rejectUnauthorized: false} : false
-
-    const db = new Client({
-        connectionString: databaseUrl,
-        ssl: ssl
-    })
-
-    try {
-        db.connect()
-        console.log('Connected')
-        db.query('create table if not exists osm_user ( name text not null unique, connections int default 0, changes int default 0, changesets int default 0, created_at TIMESTAMPTZ DEFAULT Now());')
-        console.log('Table osm_user created or existing')
-    } catch(e) {
-        console.log('An error occured')
-        console.log(e)
-    }
+        db = new Client({
+            connectionString: databaseUrl,
+            ssl: ssl
+        })
+    
+        try {
+            db.connect()
+            console.log('Connected')
+            db.query('create table if not exists osm_user ( name text not null unique, connections int default 0, changes int default 0, changesets int default 0, created_at TIMESTAMPTZ DEFAULT Now());')
+            console.log('Table osm_user created or existing')
+        } catch(e) {
+            console.log('An error occured')
+            console.log(e)
+            db = null
+        }
+    }    
 
     const request = require('request')
     const osmtogeojson = require('osmtogeojson')
@@ -131,8 +134,10 @@ module.exports = function(app, databaseUrl, prod) {
         } else {
             let name = req.query.name
             console.log('get /connect name=' + name)
-            db.query('insert into osm_user(name) values($1) on conflict (name) do nothing;', [name])
-            db.query('update osm_user set connections = connections + 1 where name like $1;', [name])
+            if(db) {
+                db.query('insert into osm_user(name) values($1) on conflict (name) do nothing;', [name])
+                db.query('update osm_user set connections = connections + 1 where name like $1;', [name])
+            }
             
             res.json({status: 200})
         }
@@ -153,7 +158,7 @@ module.exports = function(app, databaseUrl, prod) {
             let name = req.query.name
             let changes = req.query.changes
             console.log('get /stat-changes name=' + name + ' changes=' + changes)
-            db.query('update osm_user set changes = changes + $2, changesets = changesets + 1 where name like $1;', [name, changes])
+            if(db) db.query('update osm_user set changes = changes + $2, changesets = changesets + 1 where name like $1;', [name, changes])
             
             res.json({status: 200})
         }
@@ -172,20 +177,22 @@ module.exports = function(app, databaseUrl, prod) {
         })
         var html = "<!doctype html><html><head><title>Tumulus stats</title></head><body><h1>OSM users</h1><table border=\"1\"><tr><th>name</th><th>created at</th><th>connections</th><th>changesets</th><th>changes</th></tr>"
 
-        db.query('select name, created_at, connections, changesets, changes from osm_user order by changes desc;', (err, sel) => {
-            if(err) {
-                console.error(err.message)
-            }
-            for(var r in sel.rows) {
-                const row = sel.rows[r]
-                html += "<tr><td>" + row.name + "</td><td>" + row.created_at + "</td><td>" + row.connections + "</td><td>" + row.changesets + "</td><td>" + row.changes + "</td></tr>"
-            }
-            html += "</table>"
-
-
-            html += "</body></html>"
-            res.write(html, "utf-8")
-            res.end()
-        })
+        if(db) {
+            db.query('select name, created_at, connections, changesets, changes from osm_user order by changes desc;', (err, sel) => {
+                if(err) {
+                    console.error(err.message)
+                }
+                for(var r in sel.rows) {
+                    const row = sel.rows[r]
+                    html += "<tr><td>" + row.name + "</td><td>" + row.created_at + "</td><td>" + row.connections + "</td><td>" + row.changesets + "</td><td>" + row.changes + "</td></tr>"
+                }
+                html += "</table>"
+    
+    
+                html += "</body></html>"
+                res.write(html, "utf-8")
+                res.end()
+            })
+        }
     })
 }
